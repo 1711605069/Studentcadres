380b7e049664150908f732ba0ce5aedbb49d8252
1711605059 <15296754653@qq.com>
2019-01-04 21:24:18 +0800

    新建了一个Infor类

---
+++ app/src/main/java/com/example/anywayany/studentcadres/Infor.java
@@ -0,0 +1,55 @@
+package com.example.anywayany.studentcadres;
+
+public class Infor {
+    private int _id;
+    private String name;
+    private String phone;
+    private String position;
+    private String department;
+    private String telephone;
+
+
+
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() { return phone; }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartment(){ return department;};
+
+    public void setDepartment(String department){ this.department = department;}
+
+    public String getTelephone(){ return telephone;};
+
+    public void setTelephone(String telephone){ this.telephone = telephone;}
+
+
+
+
+}
